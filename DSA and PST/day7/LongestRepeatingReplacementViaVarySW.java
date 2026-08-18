614b6be4c5f124290f1b3a81a1284525e892bb19
razzaksr <razzaksr@gmail.com>
2026-08-18 13:13:30 +0530

    dsa day7 Variable window via heap

--- DSA and PST/day7/LongestRepeatingReplacementViaVarySW.java
+++ DSA and PST/day7/LongestRepeatingReplacementViaVarySW.java
@@ -1,15 +1,19 @@
 package day7;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LongestRepeatingReplacementViaVarySW {
     public static int characterReplacement(String txt, int k) {
         int max = 0, maxfreq = 0, start = 0;
-        int[] arr = new int[26];
+        Map<Character,Integer> heap = new HashMap<>();
         for(int end=0;end<txt.length();end++){
-            int index = txt.charAt(end)-'A';
-            arr[index]++;
-            maxfreq = Math.max(arr[index],maxfreq);
+            char key = txt.charAt(end);
+            heap.put(key, heap.getOrDefault(key, 0)+1);
+            maxfreq = Math.max(heap.get(key),maxfreq);
             while((end-start+1)-maxfreq>k){
-                arr[txt.charAt(start)-'A']--;
+                char sKey = txt.charAt(start);
+                heap.put(sKey, heap.get(sKey)-1);
                 start++;
             }
             max = Math.max(max,end-start+1);
